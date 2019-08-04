cb311ac379e0cef06d6a7acc0c209224d65208d7
Charlie Windolf <cwindolf95@gmail.com>
2019-08-04 19:48:14 -0400

    uh... it works?

--- alphdbn.py
+++ alphdbn.py
@@ -16,26 +16,29 @@ def adjoint(filters):
 def conv(K, x):
     """strided on width, fixed on height"""
     h, f, H = K.shape
-    w, h_ = x.shape
+    print(x)
+    b, w, h_ = x.shape
+    print(b, w, h_)
     assert h == h_, f"need (x height:{h_})==(K in height: {h})"
-    # wh -> 1hw1
-    x = tf.transpose(x, (1, 0))
-    x = tf.expand_dims(x, 0)
+    # bwh -> bhw1
+    x = tf.transpose(x, (0, 2, 1))
     x = tf.expand_dims(x, -1)
     # hfH -> hf1H
     K = tf.expand_dims(K, 2)
-    # 1hw1, hf1H -> 1(h-h+1==1)(w-f+1)H
+    # bhw1, hf1H -> b(h-h+1==1)(w-f+1)H
     res = tf.nn.conv2d(x, K, padding="VALID")
-    # 11(w-f+1)H -> (w-f+1)H
+    # b1(w-f+1)H -> b(w-f+1)H
     res = tf.squeeze(res)
     # let's make sure...
-    assert res.shape == (w - f + 1, H)
+    assert res.shape == (b, w - f + 1, H)
     return res
 
 
 def conv_adj(K, x):
     _, f, _ = K.shape
-    return conv(adjoint(K), tf.pad(x, [(f - 1, f - 1), (0, 0)]))
+    res = conv(adjoint(K), tf.pad(x, [(0, 0), (f - 1, f - 1), (0, 0)]))
+    print(f'conv_adj: K.shape={K.shape}, x.shape={x.shape}, res.shape={res.shape}')
+    return res
 
 
 KernelResults = namedtuple("KernelResults", ["target_log_prob"])
@@ -108,10 +111,13 @@ AlphRBM = namedtuple(
         "beta",
         "stimulus",
         "filters",
+        "biases",
         "vis_sample",
         "hid_sample",
+        "hid_mean",
         "cost",
         "train_op",
+        "vis_sample_chain",
     ],
 )
 
@@ -121,28 +127,30 @@ def net(
     seq_len,
     filter_len,
     hid_height,
-    hid_p_target=0.1,
-    lr=0.01,
-    momentum=0.1,
-    wd=0.1,
+    batch_size,
+    sparse_hid=0.0,
+    lr=5e-5,
+    momentum=0.01,
+    wd=1e-5,
 ):
     """Make an RBM with categorical inputs
     """
     # shapes ----------------------------------------------------------
-    vis_shape = (seq_len,)
+    vis_shape = (batch_size, seq_len,)
     filter_shape = (alphabet_size, filter_len, hid_height)
-    hid_shape = (seq_len - filter_len + 1, hid_height)
+    hid_shape = (batch_size, seq_len - filter_len + 1, hid_height)
     nhid = (seq_len - filter_len + 1) * hid_height
 
     # variables -------------------------------------------------------
     filters_ = tf.get_variable(
         "filters0",
-        initializer=np.random.normal(size=filter_shape).astype(np.float32),
+        initializer=np.random.normal(scale=0.1, size=filter_shape).astype(np.float32),
         trainable=False,
     )
-    hiddens_rv = tfd.Bernoulli(
-        probs=np.full(hid_shape, hid_p_target).astype(np.float32),
-        dtype=tf.float32,
+    biases_ = tf.get_variable(
+        "biases0",
+        initializer=np.random.normal(scale=0.1, size=hid_shape).astype(np.float32),
+        trainable=False,
     )
 
     # params ----------------------------------------------------------
@@ -151,29 +159,42 @@ def net(
     # samplers --------------------------------------------------------
     def vis_given_hid(hiddens_):
         in_logits_ = beta_ * conv_adj(filters_, hiddens_)
+        print('vis_given_hid, in_logits_', in_logits_.shape)
         in_cat_rv = tfd.Categorical(logits=in_logits_)
-        return in_cat_rv.sample()
+        vis_sample_ = in_cat_rv.sample()
+        print('vis_given_hid, vis_sample_', vis_sample_.shape)
+        return vis_sample_
 
     def hid_given_vis(visibles_):
         onehots_ = tf.one_hot(visibles_, alphabet_size)
-        hid_logits_ = beta_ * conv(filters_, onehots_)
+        hid_logits_ = beta_ * (conv(filters_, onehots_) + biases_)
         hid_ber_rv = tfd.Bernoulli(logits=hid_logits_, dtype=tf.float32)
         hid_sample_ = hid_ber_rv.sample()
         return hid_sample_
 
-    # log likelihood --------------------------------------------------
-    def energy(visibles_, hiddens_, filters_):
+    # log likelihoods -------------------------------------------------
+    def energy(visibles_, hiddens_, filters_, biases_):
         onehots_ = tf.one_hot(visibles_, alphabet_size)
-        in_logits_ = conv_adj(filters_, hiddens_)
-        model_energy_ = tf.reduce_sum(in_logits_ * onehots_)
-        filter_energy_ = wd * tf.reduce_sum(tf.square(filters_))
-        sparsity_energy_ = -tf.reduce_sum(hiddens_rv.log_prob(hiddens_))
+        hid_logits_ = beta_ * (conv(filters_, onehots_) + biases_)
+        model_energy_ = -tf.reduce_sum(hid_logits_ * hiddens_)
+        # filter_energy_ = wd * tf.reduce_sum(tf.square(filters_))
+        # sparsity_energy_ = sparse_hid * tf.reduce_sum(tf.abs(hiddens_))
         # print_energies_ = tf.print(
         #     "model_energy_", model_energy_,
         #     "filter_energy_", filter_energy_)
         # with tf.control_dependencies([print_energies_]):
-        total_energy = filter_energy_ + model_energy_ + sparsity_energy_
+        total_energy = model_energy_
         return total_energy
+
+    def free_energy_vis(visibles_, filters_, biases_):
+        return -tf.reduce_sum(
+            tf.log(1.0
+                   + tf.exp(
+                    beta_ * (
+                        biases_
+                        + conv(
+                            filters_,
+                            tf.one_hot(visibles_, alphabet_size))))))
 
     # hallucination ops -----------------------------------------------
     # pass in a stimulus...
@@ -182,7 +203,7 @@ def net(
     init_hids_ = tf.zeros(hid_shape, name="init_hids")
     # get sample
     gk = GibbsKernel(
-        energy, [vis_given_hid, hid_given_vis], extra_args=[filters_]
+        energy, [vis_given_hid, hid_given_vis], extra_args=[filters_, biases_]
     )
     # This doesn't quite work yet -- some shape problem with mh's one_step.
     # mhk = tfp.mcmc.MetropolisHastings(inner_kernel=gk)
@@ -190,11 +211,24 @@ def net(
         num_results=1,
         current_state=[stimulus_, init_hids_],
         kernel=gk,
-        num_burnin_steps=100,
+        num_burnin_steps=0,
         trace_fn=None,
         parallel_iterations=1,
     )
-    vis_sample_, hid_sample_ = res
+    long_chain_res = tfp.mcmc.sample_chain(
+        num_results=128,
+        current_state=[stimulus_, init_hids_],
+        kernel=gk,
+        num_burnin_steps=0,
+        num_steps_between_results=2,
+        trace_fn=None,
+        parallel_iterations=1)
+    print('lcr', long_chain_res)
+    long_chain_vis = tf.squeeze(long_chain_res[0])
+    print('mcmcres', res)
+    vis_sample_, hid_sample_ = (tf.squeeze(s) for s in res)
+
+    print('mcmcres vis_sample_', vis_sample_)
 
     # training ops ----------------------------------------------------
     # sample
@@ -204,10 +238,23 @@ def net(
         dtype=tf.float32,
     )
     hid_sample_ = hid_rv.sample()
+    hid_mean_ = tf.reduce_mean(hid_sample_)
     # do some gradient descent on filters with vis/hids fixed
     opt = tf.train.MomentumOptimizer(lr, momentum)
-    cost_ = energy(stimulus_, hid_sample_, filters_)
-    train_op_ = opt.minimize(cost_, var_list=[filters_])
+    cost_ = (free_energy_vis(stimulus_, filters_, biases_)
+            - free_energy_vis(vis_sample_, filters_, biases_)) / batch_size
+    cost_ = cost_ + wd * (tf.reduce_sum(tf.square(filters_))
+                         + tf.reduce_sum(tf.square(biases_)))
+    train_op_ = opt.minimize(cost_, var_list=[filters_, biases_])
+
+    # hallucination function ------------------------------------------
+    def vis_sample_chain(sess, beta=1.0):
+        start_vis = np.random.randint(alphabet_size, size=(batch_size, seq_len))
+        vis_samples = sess.run(long_chain_vis, feed_dict={
+                stimulus_: start_vis,
+                beta_: beta,
+            })
+        return vis_samples
 
     # build return pack -----------------------------------------------
     net = AlphRBM(
@@ -217,9 +264,12 @@ def net(
         beta=beta_,
         stimulus=stimulus_,
         filters=filters_,
+        biases=biases_,
         vis_sample=vis_sample_,
         hid_sample=hid_sample_,
         cost=cost_,
+        hid_mean=hid_mean_,
         train_op=train_op_,
+        vis_sample_chain=vis_sample_chain,
     )
     return net
--- traindbn.py
+++ traindbn.py
@@ -1,11 +1,17 @@
 import numpy as np
+import os.path
 import tensorflow as tf
 import string, itertools
+import gensim.utils
 from alphdbn import net
+import matplotlib.pyplot as plt
 
 
 seq_len = 128
-alphabet = string.ascii_lowercase + '., '
+n_hid = 1024
+batch_size = 16
+fw = 7
+alphabet = string.ascii_lowercase + '\'\"., '
 alphabet_size = len(alphabet)
 itoa = dict(enumerate(alphabet))
 atoi = dict((a, i) for i, a in enumerate(alphabet))
@@ -15,13 +21,15 @@ lpc_txt = 'lpc.txt'
 
 # -------------------------- data processing --------------------------
 
-def munge(aloloc, min_len=7):
+def munge(aloloc):
     aloi = []
     for aloc in aloloc:
-        for c in aloc.strip().lower():
-            if c in atoi:
-                aloi.append(atoi[c])
-        aloi.append(atoi[' '])
+        aloc = gensim.utils.deaccent(aloc)
+        if aloc.strip():
+            for c in ' '.join(aloc.lower().strip().split()):
+                if c in atoi:
+                    aloi.append(atoi[c])
+            aloi.append(atoi[' '])
     return np.array(aloi).astype(np.int32)
 
 
@@ -39,27 +47,60 @@ if __name__ == '__main__':
 
     # ------------------------------ train ----------------------------
 
-    dbn = net(alphabet_size, seq_len, 5, 128)
+    dbn = net(alphabet_size, seq_len, fw, n_hid, batch_size)
     init_op_ = tf.global_variables_initializer()
 
     with tf.Session() as sess:
-        sess.run(init_op_)
-        for i in range(10000):
-            rand_start = np.random.randint(dem_corpus.size - seq_len)
-            cost, _ = sess.run([dbn.cost, dbn.train_op], feed_dict={
-                    dbn.beta: 1.0,
-                    dbn.stimulus: dem_corpus[rand_start:rand_start + seq_len],
-                })
-            print(cost)
+        if os.path.exists('model.npz'):
+            with np.load('model.npz') as model:
+                dbn.filters.load(model['filters'], sess)
+                dbn.biases.load(model['biases'], sess)
 
-            if not i % 250:
-                # run sample chain
-                sample = dem_corpus[rand_start:rand_start + seq_len]
-                print('start:')
-                print(''.join(itoa[i] for i in sample))
-                for j in range(5):
-                    sample, = sess.run(dbn.vis_sample, feed_dict={
-                            dbn.beta: 2.0,
-                            dbn.stimulus: sample,
-                        })
-                    print(''.join(itoa[i] for i in sample))
+            task = input('train/sample: ')
+            assert task in ('train', 'sample')
+
+        if task == 'train':
+            sess.run(init_op_)
+            for i in range(100000):
+                rand_starts = np.random.randint(dem_corpus.size - seq_len, size=batch_size)
+                batch_getter = np.r_[('0,2', *tuple(slice(s, s + seq_len) for s in rand_starts))]
+                cost, hm, _ = sess.run([dbn.cost, dbn.hid_mean, dbn.train_op], feed_dict={
+                        dbn.beta: np.sqrt(0.4),
+                        dbn.stimulus: dem_corpus[batch_getter],
+                    })
+
+                if not i % 1000:
+                    print(cost, hm)
+                    # run sample chain
+                    samples = dem_corpus[batch_getter]
+                    results = [samples]
+                    for j in range(5):
+                        samples = sess.run(dbn.vis_sample, feed_dict={
+                                dbn.beta: 1.0,
+                                dbn.stimulus: samples,
+                            })
+                        results.append(samples)
+                    for j in range(batch_size):
+                        print('start:')
+                        for sample in results:
+                            print(''.join(itoa[i] for i in sample[j]))
+
+                    filters, biases = sess.run([dbn.filters, dbn.biases])
+                    plt.subplot(121)
+                    plt.hist(filters.ravel())
+                    plt.subplot(122)
+                    plt.hist(biases.ravel())
+                    plt.savefig('hi.png')
+                    plt.close('all')
+
+                    np.savez('model.npz', filters=filters, biases=biases)
+        else:
+            while True:
+                beta = input('beta pls: ')
+                beta = float(beta)
+                print(beta)
+                results = dbn.vis_sample_chain(sess, beta)
+                for j in range(batch_size):
+                    print('start:')
+                    for sample in results:
+                        print(''.join(itoa[i] for i in sample[j]))
